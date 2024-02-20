229fbc71b71f950dc2406c411c7322c60a5cb88e
Tjosse97 <10734149@uvu.edu>
2024-02-20 16:59:36 -0700

    redo navigation

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer or kart stats")
 
-link = '[To my Github Pages Site] (http://tjosse97/github_assignment/index.html)'
+link = '[To my Github Pages Site] (http://127.0.0.1:5500/github_assignment/index.html)'
 st.markdown(link, unsafe_allow_html=True)
